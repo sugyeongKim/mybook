01c4e4a597217206043fb909bc178d4ded3e5ace
sugyeong Kim <s2018s25@e-mirim.hs.kr>
2020-05-19 22:09:52 +0900

    4번 문제. book 클래스 생성

--- mybook/Book.cs
+++ mybook/Book.cs
@@ -8,6 +8,14 @@ namespace mybook
 {
     class Book
     {
-       
+        private string name = "PHP 프로그래밍 입문";
+        private string first_date = "2013년 5월 20일";
+        private string author = "황재호";
+        private string publisher = "김태헌";
+        private string company = "한빛아카데미(주)";
+        private string director = "김현용";
+        private string planner = "김이화";
+        private string editor = "김이화";
+        private string designer = "여동일";
     }
 }
